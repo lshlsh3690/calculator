6184d057ae6fd1860ffd3eab6d248f1d42431141
최규태 <kuitae0725@gmail.com>
2022-10-07 16:28:01 +0900

    make the conflict

--- main.c
+++ main.c
@@ -9,6 +9,8 @@ int main(int argc, char* argv[]){
   int number1, number2, result;
   char operator;
 
+
+  printf( "make the conflict"  );
   printf("please enter a expression : ");
   scanf("%d %c %d", &number1 &operator, &number2);
 
