b63fa0adf9e6cb294d355c14e863fb5acc74086e
이성훈 <lshlsh3690@gmail.com>
2022-10-07 16:50:40 +0900

    Revert "2"
    This reverts commit 2c139a47fd8e49e258d48fe76027adc3a9161eaa.
    
    # Conflicts:
    #	main.c

--- main.c
+++ main.c
@@ -14,9 +14,6 @@ int main(int argc, char* argv[]){
   int number1, number2, result;
   char operator;
   //1
-  //2
-  //3
-
   printf("please enter a expression : (ex>1,2)");
   printf("please enter a expression : ");
   scanf("%d %c %d", &number1 &operator, &number2);
